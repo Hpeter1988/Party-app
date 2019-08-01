c93ae5f5970bbab58ef311bd9560c3f2e779bab2
zsofi777 <kovagozsofia@gmail.comgit config --listk>
2019-08-01 23:22:20 +0200

    Landing, welcome and test pages css

--- src/Components/Welcome.js
+++ src/Components/Welcome.js
@@ -7,9 +7,10 @@ const Welcome = () => (
       <h1>Welcome to Party Rater!</h1>
       <p>You will get ratings about</p>
     </div>
+    <div className="cdc">
     <div className="welcome">
       <div className="img"><img src="https://via.placeholder.com/100x100" alt="Pic comes here" /></div>
-      <h4 className="titles">the WIBE</h4>
+      <h4 className="titles">the VIBE</h4>
     </div>
     <div className="welcome">
       <div className="img"><img src="https://via.placeholder.com/100x100" alt="Pic comes here" /></div>
@@ -20,6 +21,7 @@ const Welcome = () => (
       <h4 className="titles">and the MUSIC</h4>
     </div>
   </div>
+    </div>
 );
 
 export default Welcome;
